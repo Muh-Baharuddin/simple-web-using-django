1281c149f29a83689715bae655121f128042f459
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-09-14 12:11:41 +0700

    create profile and contact page

--- kerangka/views.py
+++ kerangka/views.py
@@ -10,3 +10,15 @@ def home(request):
     'title':title
   }
   return render(request, 'home.html', context)
+
+def profile(request):
+  context={
+    'title':title
+  }
+  return render(request, 'profile.html', context)
+
+def contact(request):
+  context={
+    'title':title
+  }
+  return render(request, 'contact.html', context)
